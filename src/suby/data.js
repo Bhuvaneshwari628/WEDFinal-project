40e665e34379d43f42ed74fb5e32031c37522565
tallasameethkumargoud <145166124+tallasameethkumargoud@users.noreply.github.com>
2024-12-05 16:53:33 -0500

    inital commit

---
+++ src/suby/data.js
@@ -0,0 +1,30 @@
+export const itemData = [{
+        item_img: "/assets/item/candy.jpeg"
+
+    },
+    {
+        item_img: "/assets/item/candy.jpeg"
+    },
+    {
+        item_img: "/assets/item/creamstone.jpg"
+    },
+    {
+        item_img: "/assets/item/dryfruit.jpeg"
+    },
+    {
+        item_img: "/assets/item/ferro.jpeg"
+    },
+    {
+        item_img: "/assets/item/hazle.jpeg"
+    },
+    {
+        item_img: "/assets/item/creamstone.jpg"
+    },
+    {
+        item_img: "/assets/item/creamstone.jpg"
+    },
+    {
+        item_img: "/assets/item/dryfruit.jpeg"
+    },
+
+]
